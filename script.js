6c17bf15cb0d147f1ff34c4dd89e446f4ea5c178
ShwetaP21 <shwetapachpute@somaiya.edu>
2025-09-29 23:24:57 +0530

    draw

--- script.js
+++ script.js
@@ -2,77 +2,79 @@ let boxes = document.querySelectorAll(".box");
 let resetButton = document.querySelector(".reset-button");
 let newButton = document.querySelector(".new-button");
 let winnerText = document.querySelector(".winnerText");
-const msg = document.querySelector(".message")
-const heading = document.querySelector(".heading")
+const msg = document.querySelector(".message");
+const heading = document.querySelector(".heading");
 let playerO = true;
+let moves = 0;
+let gameActive = true;
 
 const winningPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8] 
-]
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
-const disableButtons = () =>{
-    console.log("inside diable buttons");
-    
-    boxes.forEach((box)=>{
-        box.disabled = true;
-    })
-}
+const disableButtons = () => {
+  console.log("inside diable buttons");
 
-const checkWinner = () =>{
-    console.log("inside check patterns");
-    
-    winningPatterns.forEach((pattern)=>{ 
-            const tex1 = boxes[pattern[0]].innerText;
-            const tex2 = boxes[pattern[1]].innerText;
-            const tex3 = boxes[pattern[2]].innerText;
-
-        if(tex1!= "" && tex2 != "" && tex3!=""){
-            if(tex1 === tex2 &&  tex2 === tex3){
-                winnerText.classList.remove("hide")
-                heading.innerText = `Congratulations!\n ${tex1} has won`  
-                disableButtons();  
-           }
-        }
-          
-    })
-}
-
-const resetButtons =()=>{
- heading.innerText = `Play Now !!` 
- playerO = true
- boxes.forEach((box)=>{
-    box.innerText = ""
-    box.disabled = false
- }) 
-}
-const handleClick = boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (playerO) {
-      box.innerText = "O";
-      playerO = false;
-    } else {
-      box.innerText = "X";
-      playerO = true;
-    }
+  boxes.forEach((box) => {
     box.disabled = true;
+  });
+};
+
+const checkWinner = () => {
+  console.log("inside check patterns");
+
+  winningPatterns.forEach((pattern) => {
+    const tex1 = boxes[pattern[0]].innerText;
+    const tex2 = boxes[pattern[1]].innerText;
+    const tex3 = boxes[pattern[2]].innerText;
+
+    if (tex1 != "" && tex2 != "" && tex3 != "") {
+      if (tex1 === tex2 && tex2 === tex3) {
+        console.log("inside in");
+        
+        winnerText.classList.remove("hide");
+        heading.innerText = `Congratulations!\n ${tex1} has won`;
+        disableButtons();
+        return;
+      }else if (moves == 9) {
+        heading.innerText = `It's a Draw`;
+        return;
+      }
+    }
+  });
+};
+
+const resetButtons = () => {
+  heading.innerText = `Play Now !!`;
+  playerO = true;
+  moves = 0;
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.disabled = false;
+  });
+};
+
+const handleClick = boxes.forEach((box) => {
+  if (!gameActive || box.innerText != "") {
+    return;
+  }
+
+  box.addEventListener("click", () => {
+    box.innerText = playerO ? "O" : "X";
+    playerO = !playerO;
+    box.disabled = true;
+    moves++;
     checkWinner();
   });
 });
 
-resetButton.addEventListener("click", ()=>{
-    console.log("inside reset button");
-    resetButtons();
-})
-
-newButton.addEventListener("click", ()=>{
-    console.log("inside new button");
-    
-    resetButtons();
-})
+[resetButton, newButton].forEach((button) => {
+  button.addEventListener("click", resetButtons);
+});
